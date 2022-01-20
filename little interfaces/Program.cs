353f7577a7d89d4c80ff2166256790be0f8e202d
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-20 17:38:26 +0800

    ternyata bisa instantiate object with interface, as long as the corresponding class that implement the interface exist

--- little interfaces/Program.cs
+++ little interfaces/Program.cs
@@ -7,12 +7,24 @@ namespace little_interfaces
         static void Main(string[] args)
         {
             //CowokTinggi cowokTinggi = new CowokTinggi() { Nama = "Shelby", Tinggi = 78 };
-            CowokTinggi cowokTinggi = new CowokTinggi("Deddy", 78);
+            //CowokTinggi cowokTinggi = new CowokTinggi("Deddy", 78);
+            //CowokTinggi badut = new CowokTinggi("Morgan", 77);
+            //CowokTinggi badut = new CowokTinggi("Morgan", 77);
+            //IBadut budi = badut;
+            IBadut anto = new CowokTinggi("Antoho", 88);
 
-            cowokTinggi.CeritakanDiriAnda();
+            //cowokTinggi.CeritakanDiriAnda();
+            //badut.CeritakanDiriAnda();
+            // budi.CeritakanDiriAnda(); <-- INVALID
+            CowokTinggi badu = (CowokTinggi)anto;
 
-            Console.WriteLine($"Cowok tinggi ini punya {cowokTinggi.HalLucu}");
-            cowokTinggi.Bunyi();
+            badu.CeritakanDiriAnda();
+
+            //Console.WriteLine($"Cowok tinggi ini punya {budi.HalLucu}");
+            //budi.Bunyi();
+
+            Console.WriteLine($"Cowok tinggi ini punya {badu.HalLucu}");
+            badu.Bunyi();
         }
     }
 }
